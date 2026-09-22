fa3ec929c0e9bf9aa86bd3821129eaa3a0285da6
e94788773-netizen <e94788773@gmail.com>
2026-09-23 02:35:56 +0900

    Update test_api.py

--- test_api/test_api.py
+++ test_api/test_api.py
@@ -1,26 +1,49 @@
+import pytest
 import requests
 import sqlite3
-def test_get_post():
-    responce = requests.get("https://jsonplaceholder.typicode.com/posts/1")
-    data = responce.json()
-    assert responce.status_code == 200
-    assert "title" in data
-    assert "body" in data
-    assert data["id"] == 1
-    assert "userId" in data
 def test_save_post_to_db():
     conn = sqlite3.connect("test.db")
     cursor = conn.cursor()
-    cursor.execute("""CREATE TABLE IF NOT EXISTS posts (
+    cursor.execute("""CREATE TABLE IF NOT EXISTS saved_posts (
         id INTEGER PRIMARY KEY,
-        title TEXT)""")
+        title TEXT
+    )""")
     conn.commit()
-    get_post = requests.get("https://jsonplaceholder.typicode.com/posts/1")
-    data = get_post.json()
+    responce = requests.get("https://jsonplaceholder.typicode.com/posts/1")
+    assert responce.status_code == 200
+    data = responce.json()
+    assert "title" in data
     title = data["title"]
-    cursor.execute("INSERT INTO posts (title) VALUES (?)", (title,))
+    cursor.execute("INSERT INTO saved_posts (title) VALUES (?)", (title,))
     conn.commit()
-    cursor.execute("SELECT title FROM posts WHERE title = (?)", (title,))
+    cursor.execute("SELECT title FROM saved_posts WHERE title = ?", (title,))
     row = cursor.fetchone()
     assert row is not None
     assert row[0] == title
+    conn.close()
+def test_create_post():
+    conn = sqlite3.connect("post.db")
+    cursor = conn.cursor()
+    cursor.execute("""CREATE TABLE IF NOT EXISTS posts (
+        id INTEGER PRIMARY KEY,
+        title TEXT
+    )""")
+    
+    new_post = {
+            "title": "Мой тестовый пост",
+            "body": "скибиди боб",
+            "userId": 1
+        }
+    responce = requests.post("https://jsonplaceholder.typicode.com/posts", json=new_post)
+    assert responce.status_code == 201
+    data = responce.json()
+    post_id = data["id"]
+    title = data["title"]
+    cursor.execute("INSERT OR IGNORE INTO posts (id, title) VALUES (?,?)", (post_id, title))
+    conn.commit()
+    cursor.execute("SELECT title FROM posts WHERE id = ?", (post_id,))
+    row = cursor.fetchone()
+    assert row is not None
+    assert row[0] == title
+    conn.close()
+    
